61c55d8851a6db41706e7d9c0456daedf75dd79c
Mateus Bizzo <bizzo.mateus@gmail.com>
2020-04-14 10:40:56 -0300

    crop and save image exercise

---
+++ exercise_1.py
@@ -0,0 +1,24 @@
+#crop and save faces from images
+import cv2
+import matplotlib.pyplot as pyplot
+
+from os import listdir, path, makedirs, getcwd
+from os.path import isfile, join
+
+path = 'ex_images'
+images_path = [join(path,f) for f in listdir(path) if isfile(join(path, f))]
+
+classifier = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_default.xml')
+
+count = 0
+# for every image: open,
+for image in images_path:
+    img = cv2.imread(image)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = classifier.detectMultiScale(gray, 1.3, 5)
+    
+    for (x, y, w, h) in faces:
+        count += 1
+        img_roi = img[y:y+h, x:x+w]
+        cv2.imwrite(join(path,'cropped','img_'+str(count)+'.png'), img_roi)
+        
--- findFace.py
+++ findFace.py
@@ -6,18 +6,18 @@ import matplotlib.pyplot as plt
 img = cv2.imread('images/px-show.jpg')
 img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 img_gray = cv2.imread('images/px-show.jpg', 0)
-copy = img_rgb.copy()
 
 #HaarCascade cassifier
 classifier = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_default.xml')
 faces = classifier.detectMultiScale(img_gray, 1.3, 5)
 
+#Draw a rectangle identifier
 for (x, y, w, h) in faces:
-    cv2.rectangle(copy, (x,y), (x+w, y+h), (255, 0, 0), 2)
+    cv2.rectangle(img_rgb, (x,y), (x+w, y+h), (255, 0, 0), 2)
 
 #showimg
 plt.figure(figsize=(10,10))
 plt.title("SHOW")
-plt.imshow(copy, cmap='gray')
+plt.imshow(img_rgb, cmap='gray')
 
 plt.show()
