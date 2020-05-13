97a2cb3441aa4ba381b402bb86fd15e0d32701ba
Mateus Bizzo <bizzo.mateus@gmail.com>
2020-05-12 23:26:55 -0300

    count files with enumerate starting in 1

--- exercise_1.py
+++ exercise_1.py
@@ -10,15 +10,13 @@ images_path = [join(path,f) for f in listdir(path) if isfile(join(path, f))]
 
 classifier = cv2.CascadeClassifier('classifiers/haarcascade_frontalface_default.xml')
 
-count = 0
 # for every image: open, findface, crop, resize, save
-for image in images_path:
+for i, image in enumerate(images_path, start=1):
     img = cv2.imread(image)
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = classifier.detectMultiScale(gray, 1.3, 5)
     
     for (x, y, w, h) in faces:
-        count += 1
         img_roi = img[y:y+h, x:x+w]
         img_roi = cv2.resize(img_roi, (162, 162), interpolation=cv2.INTER_LANCZOS4)
-        cv2.imwrite(join(path,'cropped','img_'+str(count)+'.png'), img_roi)
+        cv2.imwrite(join(path,'cropped','img_'+str(i)+'.png'), img_roi)
